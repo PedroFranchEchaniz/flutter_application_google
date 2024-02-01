3e0881df83b2a67c7cbc308f7884828bb78de502
franch.ecped23_trian <pedrofe.av@gmail.com>
2024-02-01 14:09:39 +0100

    lista fuentes2

--- lib/widget/fountain_list_response.dart
+++ lib/widget/fountain_list_response.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_google/models/fontain_response/fontain_response.dart';
 import 'package:flutter_application_google/models/fontain_response/result.dart';
@@ -5,15 +7,15 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-Future<List<Result>> fetchFountain() async {
+Future<FontainResponse> fetchFountain() async {
   final response = await http.get(Uri.parse(
       'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/fonts-daigua-publica-fuentes-de-agua-publica/records?limit=20'));
 
   if (response.statusCode == 200) {
-    List<dynamic> decodeJson = json.decode(response.body)['records'];
-    return decodeJson.map((json) => Result.fromJson(json)).toList();
+    Map<String, dynamic> decodedJson = json.decode(response.body);
+    return FontainResponse.fromJson(decodedJson);
   } else {
-    throw Exception('Failed to load fountains');
+    throw Exception('Failed');
   }
 }
 
@@ -25,7 +27,7 @@ class Fountains extends StatefulWidget {
 }
 
 class _FountainsState extends State<Fountains> {
-  late Future<List<Result>> fountains;
+  late Future<FontainResponse> fountains;
 
   final _initialCameraPostion = const CameraPosition(
       target: LatLng(37.38049329951381, -6.007534638184238), zoom: 15);
@@ -36,36 +38,43 @@ class _FountainsState extends State<Fountains> {
     fountains = fetchFountain();
   }
 
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: FutureBuilder<List<Result>>(
-        future: fountains,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No fountains found'));
-          } else {
-            Set<Marker> markers = snapshot.data!
-                .map((fountain) => Marker(
-                      markerId: MarkerId(fountain.objectid.toString()),
-                      position: LatLng(
-                          fountain.geoPoint2d!.lat!, fountain.geoPoint2d!.lon!),
-                    ))
-                .toSet();
+    return FutureBuilder(
+      future: fountains,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (!snapshot.hasData) {
+          return const Text('No hay datos');
+        } else {
+          final fontainList = snapshot.data!.results;
 
-            return GoogleMap(
+          return Scaffold(
+            body: GoogleMap(
+              mapType: MapType.hybrid,
               initialCameraPosition: _initialCameraPostion,
-              myLocationButtonEnabled: false,
-              markers: markers,
-            );
-          }
-        },
-      ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              markers: Set<Marker>.from(
+                fontainList!.map(
+                  (fuentes) => Marker(
+                    markerId: MarkerId(fuentes.calle.toString()),
+                    position: LatLng(
+                        fuentes.geoPoint2d!.lat!, fuentes.geoPoint2d!.lon!),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
